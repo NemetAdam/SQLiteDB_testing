61881ef0f243e539a0df5ab88ca71d5f2785c6af
Nemet Adam <nemetadam@yahoo.com>
2019-10-12 19:46:11 +0300

    Working goood and sonarqube implemented+ Espresso tests.

---
+++ app/src/androidTest/java/com/example/sqlitedb_testing/MainActivityEspressoTest.kt
@@ -0,0 +1,42 @@
+package com.example.sqlitedb_testing
+
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.rule.ActivityTestRule
+import org.junit.After
+import org.junit.Before
+
+import org.junit.Assert.*
+import org.junit.Rule
+import org.junit.Test
+import java.util.*
+
+class MainActivityEspressoTest {
+
+    @Rule
+    @JvmField var activityTestRule = ActivityTestRule(MainActivity::class.java)
+
+
+
+    @Test
+    fun testRandomNumberInput(){
+        Espresso.onView(ViewMatchers.withId(R.id.editText)).perform(ViewActions.typeText(Random().nextInt().toString()))
+    }
+
+    @Test
+    fun testRandomNumberInputClick(){
+        Espresso.onView(ViewMatchers.withId(R.id.editText)).perform(ViewActions.typeText(Random().nextInt().toString()))
+        Espresso.closeSoftKeyboard()
+        Espresso.onView(ViewMatchers.withId(R.id.buttonAdd)).perform(ViewActions.click())
+    }
+
+    @Before
+    fun setUp() {
+    }
+
+    @After
+    fun tearDown() {
+    }
+}
